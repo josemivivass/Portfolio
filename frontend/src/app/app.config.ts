4559acd07341ac4f46aceb7904835a20df8cc0b4
josemivivass <jose.miguel.vivas.sanchez@gmail.com>
2026-06-02 11:10:54 +0200

    Activar SSR con hidratacion en home y contacto

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideClientHydration } from '@angular/platform-browser';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './interceptors/auth.interceptor';
@@ -8,6 +9,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
     provideRouter(routes),
+    provideClientHydration(),
     provideHttpClient(withFetch(), withInterceptors([authInterceptor]))
   ]
 };
